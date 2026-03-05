7806749aef0051e7e463c373b3a2021a94d93297
badrneymar2010-cloud <badrneymar2010@alumnes.ilerna.com>
2026-03-05 15:39:46 +0100

    clase forat de gel
    creada la clase

---
+++ eclipse-workspace/JocDelPingu/src/board/squares/Ice_hole.java
@@ -0,0 +1,13 @@
+package board.squares;
+
+import board.Square;
+import board.SquareType;
+
+public class Ice_hole extends Square {
+
+	public Ice_hole(SquareType type) {
+		super(type);
+		// TODO Auto-generated constructor stub
+	}
+
+}
