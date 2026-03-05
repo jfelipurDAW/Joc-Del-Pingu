188303a5cbc4118f69ce9feb396e5c753e967df0
badrneymar2010-cloud <badrneymar2010@alumnes.ilerna.com>
2026-03-05 15:46:01 +0100

    clase forat de gel
    creada la clase i el atribut

---
+++ eclipse-workspace/JocDelPingu/src/board/squares/Sled.java
@@ -0,0 +1,15 @@
+package board.squares;
+
+import board.Square;
+import board.SquareType;
+
+public class Sled extends Square{
+	
+	private int casellaDesti;
+
+	public Sled(SquareType type) {
+		super(type);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
