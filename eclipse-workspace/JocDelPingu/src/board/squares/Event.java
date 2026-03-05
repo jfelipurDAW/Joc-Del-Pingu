d31bb927d0fa64cbbfa994d565d9369e73d004ad
badrneymar2010-cloud <badrneymar2010@alumnes.ilerna.com>
2026-03-05 15:54:29 +0100

    clase event
    creada

---
+++ eclipse-workspace/JocDelPingu/src/board/squares/Event.java
@@ -0,0 +1,13 @@
+package board.squares;
+
+import board.Square;
+import board.SquareType;
+
+public class Event extends Square {
+
+	public Event(SquareType type) {
+		super(type);
+		// TODO Auto-generated constructor stub
+	}
+
+}
